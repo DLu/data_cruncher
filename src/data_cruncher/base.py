b460917f14f4f33c45e4f383e7c686db12e675d4
David V. Lu <davidvlu@gmail.com>
2025-01-13 16:07:04 -0500

    Rate limiting

--- src/data_cruncher/base.py
+++ src/data_cruncher/base.py
@@ -1,7 +1,9 @@
 import click
+import datetime
 import pathlib
 import requests
 from dateutil.parser import parse as date_parse
+import time
 import yaml
 
 WIKIDATA_REST_URL = 'https://www.wikidata.org/w/rest.php/wikibase/v1'
@@ -13,6 +15,26 @@ if CONFIG_PATH.exists():
     config = yaml.safe_load(open(CONFIG_PATH))
 else:
     config = {}
+
+DEFAULT_WAIT_TIME = config.get('default_wait_time', 1)
+last_action_time = {}
+
+
+def _stall(key):
+    wait_time = config.get('wait_times', {}).get(key, DEFAULT_WAIT_TIME)
+    now = datetime.datetime.now()
+
+    if key in last_action_time:
+        delta = now - last_action_time[key]
+        delta_s = delta.total_seconds()
+
+        time_to_wait = wait_time - delta_s
+        if time_to_wait > 1e-9:
+            if config.get('verbose', True):
+                click.secho(f'Waiting {time_to_wait:.2f} seconds...')
+            time.sleep(time_to_wait)
+
+    last_action_time[key] = datetime.datetime.now()
 
 
 def _get_rest_headers():
@@ -28,6 +50,8 @@ def get_rest(url, header_fields_to_copy=[], **params):
     full_url = WIKIDATA_REST_URL + url
     if 'format' not in params:
         params['format'] = 'json'
+
+    _stall('get')
 
     resp = requests.get(
         full_url, params=params, headers=_get_rest_headers()
@@ -52,6 +76,7 @@ def _get_message(content):
 
 def post_rest(url, body):
     full_url = WIKIDATA_REST_URL + url
+    _stall('edit')
     resp = requests.post(
         full_url, json=body, headers=_get_rest_headers()
     )
@@ -63,6 +88,7 @@ def post_rest(url, body):
 
 def patch_rest(url, body):
     full_url = WIKIDATA_REST_URL + url
+    _stall('edit')
     resp = requests.patch(
         full_url, json=body, headers=_get_rest_headers()
     )
@@ -83,6 +109,7 @@ def sparql_query(qs):
     """Run a sparql query on wikidata and return the structured results"""
 
     params = {'query': qs, 'format': 'json'}
+    _stall('sparql')
     req = requests.get(WIKIDATA_SPARQL_URL, params=params, headers=_get_sparql_headers())
     if req.status_code != 200:
         raise RuntimeError(f'{req.status_code} {req.content}')
